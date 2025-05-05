4509eda23553c0ec155738335c1da811f970641c
sashmitha2005 <shrisashmithas.22aid@kongu.edu>
2025-05-06 00:35:32 +0530

    resolve2

--- Frontend/chemx/src/pages/AdminDashboard.jsx
+++ Frontend/chemx/src/pages/AdminDashboard.jsx
@@ -5,7 +5,6 @@ import {
   XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
   PieChart, Pie, Cell, Legend
 } from "recharts";
-import './AdminDashboard.css';
 
 const sampleData = {
   "past week": [
