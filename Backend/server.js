012aa5544cce40f6f7c4ec88419535b26822c212
sashmitha2005 <shrisashmithas.22aid@kongu.edu>
2025-05-08 19:39:07 +0530

    last

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/Auth');
 
 const app = express();
 
-app.use(cors({credentials:true,origin:"http://localhost:5173"}));
+app.use(cors({credentials:true,origin:"https://chemwebsite-sfu7.vercel.app"}));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
