d3fc247b4c1f496a4afd262c4e5087a9e871676e
sashmitha2005 <shrisashmithas.22aid@kongu.edu>
2025-05-21 21:45:17 +0530

    Update AxiosSetup.jsx

--- Frontend/chemx/src/axios/AxiosSetup.jsx
+++ Frontend/chemx/src/axios/AxiosSetup.jsx
@@ -1,7 +1,19 @@
 import axios from "axios"
-export const axiosClient=axios.create({
-    baseURL:import.meta.env.VITE_BASE_URL,
-     withCredentials:true
 
+export const axiosClient=axios.create({
+    baseURL:import.meta.env.VITE_BASE_URL
 })
 
+axiosClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("jwtToken");
+        console.log(token)
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
